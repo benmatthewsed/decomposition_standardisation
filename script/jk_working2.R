2bd1e4b1b8427fdb9ccb2f32420a21b3e1e98c54
Josiah King <josiahpjking@gmail.com>
2019-06-21 15:26:53 +0100

    model fit checks!

--- script/jk_working2.R
+++ script/jk_working2.R
@@ -1,10 +1,10 @@
+library(here)
 library(tidyverse)
 library(matrixStats)
 library(conflicted)
 library(combinat)
 conflict_prefer("filter", "dplyr")
-
-#devtools::install_github("josiahpjking/DasGuptR")
+#devtools::install_github("josiahpjking/DasGuptR",force=T)
 library(DasGuptR)
 
 #e.g. taken from Ben's code.
@@ -12,10 +12,38 @@ readRDS(here("data","testdf_long.RDS")) -> testdf
 readRDS(here("data","BMresult.RDS")) -> bm
 readRDS("data/testdf_3years.RDS") -> testdf3
 
+#some decomposing..
 dg2pops <- DasGupt_2pop(testdf,year,c("prev","age_str","freq","disposal_prop","crime_type_prop"))
+dg2pops_again <- DasGupt_Npop(testdf,pop=year,prev,age_str,freq,disposal_prop,crime_type_prop)
 dgtimeseries <- DasGupt_Npop(testdf3,pop=year,prev,age_str,freq,disposal_prop,crime_type_prop)
 
+####
+#MODEL OUTPUT
+####
 #get the prev factor diffs
 dgtimeseries$prev$factor_effects
 #or standardised rates
 dgtimeseries$prev$standardised_rates
+
+
+####
+#MODEL FIT!
+####
+fe<-map(dgtimeseries,"factor_effects")  # extract factor effects 
+fe<-fe[-1] #weirdly the output seems to have an empty element at the start. we'll need to get rid of that
+map(fe,"diff1989_2011") %>% as_tibble(.,name_repair=F) %>% rowSums %>% sum(.)*10000 # get all the diffs, rowSum, then sum
+map(fe,"diff1989_2018") %>% as_tibble(.,name_repair=F) %>% rowSums %>% sum(.)*10000 
+
+#this is the original data, 
+testdf3 %>% mutate(
+  crude=prev*age_str*freq*disposal_prop*crime_type_prop   #crude rates
+) %>% 
+  group_by(year) %>% 
+  summarise(
+    cruderate=sum(crude)*10000
+  ) %>% mutate(
+    diff=c(0,diff(cruderate)), #difference between years
+    cumdiff=cumsum(diff) #cumulative differnce - gives diff to first year
+  )
+
+#mine matches?
